e4d17a21c751c92ea1342a0a770ac1a71f8ba804
shadowpluse157 <faizan.zahoor2010@gmail.com>
2026-07-15 17:17:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
-setTimeout(()=>{
+// Wait 3 seconds then show website
 
-document.querySelector(".loader").innerHTML=
+setTimeout(() => {
 
-`
+document.querySelector(".loader").style.display = "none";
 
-<h1>Welcome Ali 🤍</h1>
-
-<p>Our story begins here...</p>
-
-`;
+document.querySelector("#home").style.display = "block";
 
 },3000);
+
+
+// Button animation
+
+const btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click",()=>{
+
+alert("Welcome to our little universe 🤍❤️");
+
+});
+
+
+// Floating Hearts
+
+const hearts = document.getElementById("hearts");
+
+setInterval(()=>{
+
+const heart=document.createElement("div");
+
+heart.innerHTML="🤍";
+
+heart.style.position="absolute";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.top="100vh";
+
+heart.style.fontSize=(20+Math.random()*25)+"px";
+
+heart.style.opacity=Math.random();
+
+heart.style.transition="all 6s linear";
+
+hearts.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.style.top="-10vh";
+
+},50);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+},350);
