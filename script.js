3c204ca39f4a147743381502defbe6fd6cea467f
shadowpluse157 <faizan.zahoor2010@gmail.com>
2026-07-25 16:54:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,56 @@ will be added later...
 ❤️
 
 `;
+// =============================
+// Open Minecraft Love Book
+// =============================
+
+openBook.addEventListener("click", () => {
+
+    // Open Book
+
+    bookPage.style.display = "flex";
+
+    // Play Music (if available)
+
+    if(music){
+
+        music.play().catch(() => {});
+
+    }
+
+    // Start Typewriter
+
+    startTyping();
+
+});
+// =============================
+// Typewriter Effect
+// =============================
+
+let currentIndex = 0;
+
+function startTyping(){
+
+    typingText.innerHTML = "";
+
+    currentIndex = 0;
+
+    typeWriter();
+
+}
+
+function typeWriter(){
+
+    if(currentIndex < letter.length){
+
+        typingText.innerHTML += letter.charAt(currentIndex);
+
+        currentIndex++;
+
+        setTimeout(typeWriter, 35);
+
+    }
+
+}
 
